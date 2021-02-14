66f5d2075e7662304e6ed9b26f135d25dab80c63
Brian Holt <btholt@gmail.com>
2021-02-14 13:10:38 -0800

    jsx

---
+++ 03-jsx/src/App.js
@@ -0,0 +1,15 @@
+import { render } from "react-dom";
+import Pet from "./Pet";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
