da9eb4c13433748788f69fc1861c66299461ae09
Brian Holt <btholt@gmail.com>
2021-02-13 17:30:26 -0800

    parcel

---
+++ 02-js-tools/src/Pet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Pet({ name, animal, breed }) {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+}
