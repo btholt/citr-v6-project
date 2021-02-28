8173bd64f17f69f26812186abe568dc372593b49
Brian Holt <btholt@gmail.com>
2021-02-27 18:19:24 -0800

    context

--- 08-react-router/src/Pet.js
+++ 08-react-router/src/Pet.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Pet = (props) => {
   const { name, animal, breed, images, location, id } = props;
 
@@ -7,7 +9,7 @@ const Pet = (props) => {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -15,7 +17,7 @@ const Pet = (props) => {
         <h1>{name}</h1>
         <h2>{`${animal} — ${breed} — ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
