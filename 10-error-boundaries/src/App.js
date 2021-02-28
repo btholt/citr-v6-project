be58c48eb805b8f016c05d3d7c770061a5a4feac
Brian Holt <btholt@gmail.com>
2021-02-27 17:01:01 -0800

    error boundaries

---
+++ 10-error-boundaries/src/App.js
@@ -0,0 +1,26 @@
+import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
