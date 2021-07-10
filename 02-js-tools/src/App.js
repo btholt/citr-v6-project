1bd36d0cb612ce80f048fc537cf873afaa8fc328
Kurt King <kurtaking@gmail.com>
2021-07-10 08:34:56 -0500

    added SearchParams component

--- 02-js-tools/src/App.js
+++ 02-js-tools/src/App.js
@@ -1,15 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Sudo" animal="Dog" breed="Wheaten Terrier" />
-      <Pet name="Chloe" animal="Cat" breed="Calico" />
-      <Pet name="Allora" animal="Cat" breed="Short Haired Something" />
+      <SearchParams />
     </div>
   );
 };
---
+++ 02-js-tools/src/SearchParams.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            onChange={(e) => setLocation(e.target.value)}
+            value={location}
+            placeholder="Location"
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
